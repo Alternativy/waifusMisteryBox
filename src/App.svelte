1c2f9f83cf26224ff3b0cfce70b60cca7dc806df
Alternativy <vesdsdsesfaeaw@gmail.com>
2021-10-26 22:52:12 -0300

    uploading
    I have a mistery box and an array of seeds of waifus.
    And for make it random, a function to shuffle the array

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	const shuffleArray = array => 
+{
+
+  for (let i = array.length - 1; i > 0; i--) 
+  {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  
+}
+	let misteryBox = "A box that contains a random Waifu";
+	let waifu = "You don`t have a waifu"
+	let inventory = misteryBox
+	let posibleWaifus = [10000,10001,10002,10003,10004]
+	function openBox(){
+		if(inventory==misteryBox){
+			shuffleArray(posibleWaifus)
+		return posibleWaifus[0]
+		}else{
+			return "You don`t have any mistery Box";
+		}
+	}
+	let seed = openBox()
+	let src = "https://thisanimedoesnotexist.ai/results/psi-0.3/seed"+seed+".png";
+	let name = 'waifu';
+	misteryBox = true
+	function toggle(){
+		misteryBox = false
+	}
+</script>
+{#if misteryBox==true}
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRos8fy29m0dkiYqHDEN3vyD6BB-JJX2hqhjw&usqp=CAU" alt="caja misteriosa" on:click={toggle}>
+{/if}
+{#if misteryBox==!true}
+<img src="{src}" alt="{name}">
+{/if}
